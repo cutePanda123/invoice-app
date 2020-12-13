26ea61e4939eaea9a7b2d14bdf05ecf088a4ae7d
Peter Li <peterli1234@126.com>
2020-12-13 11:14:56 -0800

    added transaction category filter based on active tab in CreateTransaction

--- src/containers/CreateTransaction.js
+++ src/containers/CreateTransaction.js
@@ -2,29 +2,46 @@ import React from 'react';
 import CategoryPicker from '../components/CategoryPicker';
 import { Tabs, Tab } from '../components/Tabs';
 import EditTransactionForm from '../components/EditTransactionForm';
-import { testCategories } from '../testData';
 import withContext from '../WithContext';
 
 class CreateTransaction extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            activeTabIndex: 0
+        };
+    }
+
+    tabChange = (tabIndex) => {
+        this.setState({
+            activeTabIndex: tabIndex
+        });
+    }
+
+    getActiveSelectedCategoryType = (tabIndex) => {
+        return tabIndex === 0 ? "income" : "outcome";
     }
 
     render() {
+        const { categories } = this.props.data;
+        let filteredCategories = Object.keys(categories).filter((cId) => {
+            return categories[cId].type === this.getActiveSelectedCategoryType(this.state.activeTabIndex);
+        }).map(cId => categories[cId]);
         return (
             <div
                 className='create-transaction py-3 px-3 rounded mt-3'
                 style={{background: '#fff'}}
             >
                 <Tabs
-                    activeIndex={0}
-                    onTabChange={() => {}}
+                    activeIndex={this.state.activeTabIndex}
+                    onTabChange={this.tabChange}
                 >
                     <Tab>Income</Tab>
                     <Tab>Outcome</Tab>
                 </Tabs>
                 <CategoryPicker
-                    categories={testCategories}
+                    categories={filteredCategories}
                     onSelectCategory={() => {}}
                 />
                 <EditTransactionForm
